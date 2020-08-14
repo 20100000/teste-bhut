8354f91e28b37a3cf99497b5251174f33fbb173b
20100000 <tiago_honorio2010@hotmail.com>
2020-08-14 10:44:55 -0300

    add IGNORE na base de dados

--- db/bhut.sql
+++ db/bhut.sql
@@ -22,10 +22,10 @@ CREATE TABLE IF NOT EXISTS usuario
     constraint usuario_email_uindex
         unique (email)
 );
-INSERT INTO bhut.usuario (id, email, senha, nome) VALUES (1, 'tiago@teste.com', '123456', 'tiago');
-INSERT INTO bhut.usuario (id, email, senha, nome) VALUES (2, 'teste@bhut.com', '123456', 'bhut');
-INSERT INTO bhut.carro (id, marca, ano, modelo, cor, combustivel, valor) VALUES (2, 'POLO', '2000', 'gm', 'prata', 'gasolina', '5.000,00');
-INSERT INTO bhut.carro (id, marca, ano, modelo, cor, combustivel, valor) VALUES (3, 'Gol', '2010', 'GW', 'brancco', 'alcool', '22.000,00');
+INSERT IGNORE INTO bhut.usuario ( email, senha, nome) VALUES ('tiago@teste.com', '123456', 'tiago');
+INSERT IGNORE INTO bhut.usuario ( email, senha, nome) VALUES ('teste@bhut.com', '123456', 'bhut');
+INSERT IGNORE INTO bhut.carro ( marca, ano, modelo, cor, combustivel, valor) VALUES ( 'POLO', '2000', 'gm', 'prata', 'gasolina', '5.000,00');
+INSERT IGNORE INTO bhut.carro ( marca, ano, modelo, cor, combustivel, valor) VALUES ( 'Gol', '2010', 'GW', 'brancco', 'alcool', '22.000,00');
 COMMIT;
 
 
